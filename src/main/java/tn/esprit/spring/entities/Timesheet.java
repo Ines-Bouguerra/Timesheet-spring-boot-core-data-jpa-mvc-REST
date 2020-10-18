e6ffa0a415b3e8d8929494542e6ed34dd2daeb7a
Ines-Bouguerra <ines.bouguerra2207@gmail.com>
2020-10-18 21:32:25 +0100

    Timesheet Entity

---
+++ src/main/java/tn/esprit/spring/entities/Timesheet.java
@@ -0,0 +1,12 @@
+package tn.esprit.spring.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Timesheet implements Serializable {
+
+	private static final long serialVersionUID = 3876346912862238239L;
+
+}
