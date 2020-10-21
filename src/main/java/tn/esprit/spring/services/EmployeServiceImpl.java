106ed469a93b253ea3d51092fe99c9dc21878e63
aminbousbih <medamin.bousbih@gmail.com>
2020-10-22 00:47:55 +0100

    add update employee

---
+++ src/main/java/tn/esprit/spring/services/EmployeServiceImpl.java
@@ -0,0 +1,28 @@
+package tn.esprit.spring.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import tn.esprit.spring.entities.Employe;
+import tn.esprit.spring.repository.EmployeRepository;
+
+@Service 
+public class EmployeServiceImpl implements IEmployeService {
+	@Autowired
+	EmployeRepository employeRepository;
+	
+	
+	@Override
+	public int addOrUpdateEmploye(Employe employe) {
+	employeRepository.save(employe);
+	return employe.getId();
+	}
+
+	
+	
+	public int ajouterEmploye(Employe employe) {
+		employeRepository.save(employe);
+		return employe.getId();
+	}
+
+}
---
+++ src/main/java/tn/esprit/spring/services/IEmployeService.java
@@ -0,0 +1,9 @@
+package tn.esprit.spring.services;
+
+import tn.esprit.spring.entities.Employe;
+
+public interface IEmployeService {
+
+	public int addOrUpdateEmploye(Employe employe);
+	public int ajouterEmploye(Employe employe);
+}
