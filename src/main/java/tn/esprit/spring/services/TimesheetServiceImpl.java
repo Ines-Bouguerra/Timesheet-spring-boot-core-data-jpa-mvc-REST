6ad281f150a10ecf72edcb0fabc55dd8db9aae47
Ines-Bouguerra <ines.bouguerra2207@gmail.com>
2020-10-21 23:40:36 +0100

    log4j configuration

--- src/main/java/tn/esprit/spring/services/TimesheetServiceImpl.java
+++ src/main/java/tn/esprit/spring/services/TimesheetServiceImpl.java
@@ -1,5 +1,8 @@
 package tn.esprit.spring.services;
 
+import org.apache.log4j.Logger;
+
 public class TimesheetServiceImpl {
+	private static final Logger logger = Logger.getLogger(TimesheetServiceImpl.class);
 
 }
