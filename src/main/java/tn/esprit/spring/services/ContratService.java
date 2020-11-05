17dbf42d92f57f2c18aea3c21874d6b4eabf9872
Yasserbha <yasser.belhajali@esprit.tn>
2020-11-05 08:43:49 +0100

    Modification niveau log

--- src/main/java/tn/esprit/spring/services/ContratService.java
+++ src/main/java/tn/esprit/spring/services/ContratService.java
@@ -2,6 +2,7 @@ package tn.esprit.spring.services;
 
 import java.util.List;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +13,8 @@ import tn.esprit.spring.repository.ContartRepository;
 @Service
 public class ContratService implements IContratServiceImpl {
 	
+	private static final Logger logger = Logger.getLogger(ContratService.class);
+	
 	@Autowired
 	ContartRepository contartRepository ;
 	
@@ -19,27 +22,36 @@ public class ContratService implements IContratServiceImpl {
 	@Override
 	public int addOrUpdateContart(Contrat contrat) {
 		contartRepository.save(contrat);
+		logger.info("add Or Update Contart  " +contrat);
 	return contrat.getReference();
 	}
 	
 	@Override
 	public void deleteContart(Contrat contrat){
 	contartRepository.delete(contrat);
+	logger.info("delete Contart  " +contrat);
 	}
 	
 	@Override
 	public Contrat findcontart(int ref){
-		return contartRepository.findById(ref).orElse(null);
+		Contrat contart = contartRepository.findById(ref).orElse(null);
+		logger.info("find contart by ref" +contart);
+		return contart ;
 	}
 	
 	@Override
 	public List<Contrat> allContrats(){
-		return (List<Contrat>) contartRepository.findAll();
+		
+		List<Contrat> liste =	(List<Contrat>) contartRepository.findAll();
+		logger.info("liste all Contrats " +liste);
+		return liste;
 	}
 
 	@Override
 	public Contrat addContart(Contrat contrat) {		
-		return contartRepository.save(contrat);
+		contartRepository.save(contrat);
+		logger.info("Add Contrat " +contrat);
+		return contrat ;
 	}
 
 }
